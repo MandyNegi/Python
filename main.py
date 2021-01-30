e3ec70ad610a6b66b579d4104c6917786f66db8c
Mandeep negi <mandeepnegi010@gmail.com>
2021-01-30 15:47:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,56 +7,52 @@ import webbrowser as wb
 from googletrans import Translator
 from gtts import gTTS
 import os
+from pocketsphinx import pocketsphinx, Jsgf, FsgModel
 from playsound import playsound
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-      # Press Ctrl+F8 to toggle the breakpoint.
+def choose_Language(name,srcLanguage,audio):
     s1 = sr.Recognizer()
-    s2 = sr.Recognizer()
-    s3 = sr.Recognizer()
     trans = Translator()
-    # text_trn = trans.translate('Mitä sinä teet')
-    # print('After Translation ' + text_trn)
 
-    with sr.Microphone() as source:
-        print('search something')
-        print('speak now')
-        audio = s3.listen(source)
-        print(audio)
-
-    if 'youtube' in s2.recognize_google(audio):
-        s2 = sr.Recognizer()
-        url = 'https://www.youtube.com/'
+    if name in s1.recognize_google(audio):
         with sr.Microphone() as source:
-            print('search your query')
-            audio = s2.listen(source)
-            try:
-                get = s2.recognize_google(audio)
-                print(get)
-                wb.get().open_new(url+get)
-            except sr.UnknownValueError:
-                print('error')
-            except sr.RequestError as e:
-                print('failed'.format(e))
-
-    if 'type' in s1.recognize_google(audio):
-        with sr.Microphone() as source:
-            # listen for the data (load audio to memory)
-            print('speak your text')
+            print('Please speak')
             audio_data = s1.record(source, duration=5)
-            # recognize (convert from speech to text)
-            # text = s1.recognize_google(audio_data, language="es-ES")
             text = s1.recognize_google(audio_data)
             print('Before Translation '+text)
-            text_trn = trans.translate(text, dest='hi')
+            text_trn = trans.translate(text, dest='en', src=srcLanguage)
             print(text_trn.pronunciation)
             tts= gTTS(text_trn.pronunciation)
             tts.save("trs.mp3")
             playsound('trs.mp3')
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+
+    s1 = sr.Recognizer()
+    s2 = sr.Recognizer()
+    s3 = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Choose Your Language[Spanish]')
+        print('speak now')
+        audio = s3.listen(source)
+        print(s1.recognize_google(audio))
+
+    if 'Spanish':
+      choose_Language('Spanish','es',audio)
+
+    if 'English':
+      choose_Language('Spanish','en',audio)
+
+    # elif 'german':
+    #   choose_Language('Spanish','de')
+    #
+    # elif 'Spanish':
+    #   choose_Language('Spanish','es')
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
